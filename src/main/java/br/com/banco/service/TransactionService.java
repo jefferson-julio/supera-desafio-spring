adf05329f15dcaf78552fd8fc981b05336fe2789
Telérico Jones <snhr.jj@gmail.com>
2023-07-14 22:44:15 -0300

    feature: filtragem de todas as transações do banco de dados via o servico TransactionService

---
+++ src/main/java/br/com/banco/service/TransactionService.java
@@ -0,0 +1,54 @@
+package br.com.banco.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import br.com.banco.model.Transferencia;
+import br.com.banco.model.Transferencia_;
+import br.com.banco.repository.TransferenciaRepository;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private TransferenciaRepository transferenciaRepository;
+
+    public Iterable<Transferencia> search(
+        String nomeOperadorTransacao,
+        Date dataTransferenciaStart,
+        Date dataTransferenciaEnd
+    ) {
+        List<Specification<Transferencia>> specifications = new ArrayList<>();
+
+        if (nomeOperadorTransacao != null) {
+            Specification<Transferencia> spec = (root, cq, cb) ->
+                cb.equal(root.get(Transferencia_.nomeOperadorTransacao), nomeOperadorTransacao);
+            specifications.add(spec);
+        }
+
+        if (dataTransferenciaStart != null) {
+            Specification<Transferencia> spec = (root, cq, cb) ->
+                cb.greaterThanOrEqualTo(root.get(Transferencia_.dataTransferencia), dataTransferenciaStart);
+            specifications.add(spec);
+        }
+
+        if (dataTransferenciaEnd != null) {
+            Specification<Transferencia> spec = (root, cq, cb) ->
+                cb.lessThanOrEqualTo(root.get(Transferencia_.dataTransferencia), dataTransferenciaEnd);
+            specifications.add(spec);
+        }
+
+        var queryParams = specifications
+            .stream()
+            .collect(Collectors.reducing((s1, s2) -> s1.and(s2)))
+            .orElse(null);
+
+        return this.transferenciaRepository.findAll(queryParams);
+    }
+}
